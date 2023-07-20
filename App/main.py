fa5e6293b28b98a4b5b28acf7c1a4948c24f2390
JohL99 <jlamprec16@gmail.com>
2023-07-20 15:52:58 -0700

    Cleaned up main

--- App/main.py
+++ App/main.py
@@ -1,28 +1,25 @@
 from helpers import *  # noqa: F403
-import config as cf  # noqa: F403
-        
-    
+import secret as cf  # noqa: F403
+
+
 def main():
-    write("https://www.africanhut.com/collections/confectionary") # noqa: F405
-    write("https://www.africanhut.com/collections/biscuits-cookies-and-snacks") # noqa: F405
-    write("https://www.africanhut.com/collections/beverages") # noqa: F405
-    write("https://www.africanhut.com/collections/breakfast") # noqa: F405
-    write("https://www.africanhut.com/collections/baking-cooking-and-desserts") # noqa: F405
-    write("https://www.africanhut.com/collections/jams-and-spreads") # noqa: F405
-    write("https://www.africanhut.com/collections/bottled-sauces-and-condiments") # noqa: F405, E501
-    write("https://www.africanhut.com/collections/dry-sauces-gravy-herbs-and-spices") # noqa: F405, E501
-    write("https://www.africanhut.com/collections/canned-and-packaged-goods") # noqa: F405
-    write("https://www.africanhut.com/collections/household-and-beauty") # noqa: F405
-    write("https://www.africanhut.com/collections/apparel-novelties-and-kitchen-accessories") # noqa: F405, E501
-    write("https://www.africanhut.com/collections/products-of-interest-to-south-africans") # noqa: F405, E501
+    with open("urls.txt", "r") as f:
+        for line in f:
+            url = line.strip()
+            write(url)  # noqa: F405
     
     # send file in email
     message = '''
     
     Attached is the price list for African Hut.
+    
+    To run this program on your local machine see: https://github.com/JohL99/WebCrawler
+    
     - this is an automated email
+    
     '''
     sendEmail(cf.senderEmail, cf.SenderPassword, cf.receiverEmail, "Price List", message, "data.csv") # noqa: F405, E501
     print("Done")
-
-main()
+    
+if __name__ == "__main__":
+    main()
